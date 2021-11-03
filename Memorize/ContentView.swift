338533151672de2df2bc1fbed7183db06b9ff977
scottdc92 <scott.carter92@gmail.com>
2021-11-03 16:57:33 -0500

    Lecture 1 Complete

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Scott!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 5)
+            Text("Hello, World!\nWhat up.")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
